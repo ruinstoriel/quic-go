0faa281e4a773c063e0c417b6aa9d486df3cdc88
Toby <tobyxdd@gmail.com>
2023-10-29 15:04:36 -0700

    feat: rename the module

--- interop/http09/server.go
+++ interop/http09/server.go
@@ -12,7 +12,7 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/quic-go/quic-go"
+	"github.com/apernet/quic-go"
 )
 
 const h09alpn = "hq-interop"
--- sys_conn_oob_test.go
+++ sys_conn_oob_test.go
@@ -10,8 +10,8 @@ import (
 	"golang.org/x/net/ipv4"
 	"golang.org/x/sys/unix"
 
-	"github.com/quic-go/quic-go/internal/protocol"
-	"github.com/quic-go/quic-go/internal/utils"
+	"github.com/apernet/quic-go/internal/protocol"
+	"github.com/apernet/quic-go/internal/utils"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
